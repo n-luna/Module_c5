f194f3f9849552413a242fbb8e943693ac6b61fc
n-luna <dluna@mail.ru>
2025-03-10 12:25:47 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-TOKEN = '7786667442:AAHIJ7ZD12swolA70DZ0PtEQOygCFqKha6w'  #t.me/TestTelMyBot 
+TOKEN = '7786667442:AAHIJ7ZD12swolA70DZ0PtEQOygCFqKha6w'  #t.me/TestTelMyBot
+API_KEY = '076297523ef16b55b35a2e93b360b76adb3e0c9f8a182b87a7df641ef01e55c8'
 keys = {
     'евро': 'EUR',
     'доллар': 'USD',
